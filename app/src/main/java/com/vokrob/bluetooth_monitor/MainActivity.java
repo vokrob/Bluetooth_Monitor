4b8f66cc07005e27269241e81bb04e7a8655e355
vokrob <106639488+vokrob@users.noreply.github.com>
2024-10-24 20:32:43 +0700

    Switching Bluetooth on and off

---
+++ app/src/main/java/com/vokrob/bluetooth_monitor/MainActivity.java
@@ -0,0 +1,116 @@
+package com.vokrob.bluetooth_monitor;
+
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private MenuItem menuItem;
+    private BluetoothAdapter btAdapter;
+    private final int ENABLE_REQUEST = 15;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        init();
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            return insets;
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        menuItem = menu.findItem(R.id.id_bt_button);
+        setBtIcon();
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.id_bt_button) {
+            if (!btAdapter.isEnabled()) {
+                enableBt();
+            } else {
+                btAdapter.disable();
+                menuItem.setIcon(R.drawable.ic_bt_enable);
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ENABLE_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                setBtIcon();
+            }
+        }
+    }
+
+    private void setBtIcon() {
+        if (btAdapter.isEnabled()) {
+            menuItem.setIcon(R.drawable.ic_bt_disable);
+        } else {
+            menuItem.setIcon(R.drawable.ic_bt_enable);
+        }
+    }
+
+    private void init() {
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    @SuppressLint("MissingPermission")
+    private void enableBt() {
+        Intent i = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(i, ENABLE_REQUEST);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
