a9cc6336dd5b505965256f12e84cb0c51c913eef
vokrob <106639488+vokrob@users.noreply.github.com>
2024-10-27 18:36:22 +0700

    Adding a device search

--- app/src/main/java/com/vokrob/bluetooth_monitor/BtListActivity.java
+++ app/src/main/java/com/vokrob/bluetooth_monitor/BtListActivity.java
@@ -1,17 +1,26 @@
 package com.vokrob.bluetooth_monitor;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -24,16 +33,19 @@ import java.util.List;
 import java.util.Set;
 
 public class BtListActivity extends AppCompatActivity {
+    private final int BT_REQUEST_PERM = 111;
     private ListView listView;
     private BtAdapter adapter;
     private BluetoothAdapter btAdapter;
     private List<ListItem> list;
+    private boolean isBtPermissionGranted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_bt_list);
+        getBtPermission();
         init();
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -44,11 +56,30 @@ public class BtListActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+
+        IntentFilter f1 = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        IntentFilter f2 = new IntentFilter(BluetoothDevice.ACTION_BOND_STATE_CHANGED);
+
+        registerReceiver(bReceiver, f1);
+        registerReceiver(bReceiver, f2);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        unregisterReceiver(bReceiver);
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.bt_list_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
@@ -58,6 +89,7 @@ public class BtListActivity extends AppCompatActivity {
             itemTitle.setItemType(BtAdapter.TITLE_ITEM_TYPE);
             list.add(itemTitle);
             adapter.notifyDataSetChanged();
+            btAdapter.startDiscovery();
         }
 
         return super.onOptionsItemSelected(item);
@@ -94,6 +126,42 @@ public class BtListActivity extends AppCompatActivity {
             adapter.notifyDataSetChanged();
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == BT_REQUEST_PERM) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                isBtPermissionGranted = true;
+                Toast.makeText(this, R.string.permission_to_search_for_devices_has_been_granted,
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, R.string.no_permission_to_search_for_devices, Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void getBtPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    BT_REQUEST_PERM);
+        } else {
+            isBtPermissionGranted = true;
+        }
+    }
+
+    private final BroadcastReceiver bReceiver = new BroadcastReceiver() {
+        @SuppressLint("MissingPermission")
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (BluetoothDevice.ACTION_FOUND.equals(intent.getAction())) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                Toast.makeText(context, R.string.available_device + device.getName(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 }
 
 
